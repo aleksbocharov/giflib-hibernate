781a3d9314c64f39b869a669b5413a25ee64aa0f
aleksbocharov <sashok.san@gmail.com>
2016-08-16 20:26:47 +0200

    added validation message to the view

--- src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
@@ -9,12 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,7 +56,9 @@ public class CategoryController {
     @RequestMapping("categories/add")
     public String formNewCategory(Model model) {
         // TODO: Add model attributes needed for new form
-        model.addAttribute("category", new Category());
+        if (! model.containsAttribute("category")) {
+            model.addAttribute("category", new Category());
+        }
         model.addAttribute("colors", Color.values());
         return "category/form";
     }
@@ -77,8 +82,16 @@ public class CategoryController {
 
     // Add a category
     @RequestMapping(value = "/categories", method = RequestMethod.POST)
-    public String addCategory(Category category) {
+    public String addCategory(@Valid Category category, BindingResult result, RedirectAttributes redirectAttributes) {
         // TODO: Add category if valid data was received
+        if(result.hasErrors()){
+            //Include validation errors
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.category", result);
+            //Add category object if invalid data received
+            redirectAttributes.addFlashAttribute("category", category);
+            //Redirected back to the form
+            return "redirect:/categories/add";
+        }
         categoryService.save(category);
         // TODO: Redirect browser to /categories
         return "redirect:/categories";
